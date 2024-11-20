96b306bbcc0c8c94ebdf34a172517121ea49e050
iamcharlie17 <riyadhosen40@gmail.com>
2024-11-20 15:46:41 +0600

    add: Topping class

---
+++ src/Topping.java
@@ -0,0 +1,17 @@
+public class Topping {
+    private final String name;
+    private final double price;
+
+    public Topping(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
